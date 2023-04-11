44bd6962068c1eb46c555d3cddbfb432d89404f5
NOVRI RAHMAN <129195286+studyarrahman@users.noreply.github.com>
2023-04-11 22:24:06 +0700

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,7 +11,7 @@
       <div class="row">
         <div class="col-md-4">
           <div class="card mb-4">
-            <img class="card-img-top" src="/assets/images/team-member-1.jpg" alt="Team Member">
+           
             <div class="card-body">
               <h5 class="card-title">Tono</h5>
               <p class="card-text">Web Developer</p>
@@ -20,7 +20,7 @@
         </div>
         <div class="col-md-4">
           <div class="card mb-4">
-            <img class="card-img-top" src="/assets/images/team-member-2.jpg" alt="Team Member">
+          
             <div class="card-body">
               <h5 class="card-title">Samsul</h5>
               <p class="card-text">Front-end Developer</p>
@@ -29,7 +29,7 @@
         </div>
         <div class="col-md-4">
           <div class="card mb-4">
-            <img class="card-img-top" src="/assets/images/team-member-3.jpg" alt="Team Member">
+            
             <div class="card-body">
               <h5 class="card-title">Dimas</h5>
               <p class="card-text">Back-end Developer</p>
